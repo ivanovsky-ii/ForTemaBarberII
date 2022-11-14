350e81ab6a643460fc4b9e91c3822fe305f926e6
ivan <ivan@DESKTOP-K11ITDN>
2022-11-14 12:45:05 +0300

    Добавьте файлы проекта.

---
+++ BarberShopII/ActionFrames/NavigationCLass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace BarberShopII.ActionFrames  
+{
+    public static class NavigationCLass
+    {
+        public static Frame MainAction { get; set; }
+        public static Frame MenuAction { get; set; }
+    }
+}
---
+++ BarberShopII/DB/conDB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BarberShopII.DB
+{
+    public static  class conDB
+    {
+        public static BarberIIEntities bred = new BarberIIEntities();
+    }
+}
